420a7feac8b7acad84b6938a83ebb75d2ece83f0
韦畋君 <@192.168.0.123>
2016-05-23 20:45:36 +0800

    5/23

--- hardware.wx/src/script/admin.js
+++ hardware.wx/src/script/admin.js
@@ -2,22 +2,27 @@
  * New node file
  */
 
+var goods={id: '',name: '',category1: '',category2: '',price: '',dummyPrice: '',param: '',note: ''};
+
 var vm = avalon.define({
 	$id: "hardware",
 	userName: "",
 	pw: "",
 	token: "",
 	tab: "",
+	goodsTab: "list",
 	userId: 0,
 	$computed: {
 	},
 	category1: [],
 	category2: [],
+	category2Temp: [],
 	category1New: "",
 	category2New: "",
 	category1Cur: {},
 	category2Cur: {},
 	goodsList: [],
+	goods: avalon.mix({},goods),
 	login: function(){
 		vm.load(apiDomain+"admin/login.json",{"m.name": vm.userName, "m.pw": vm.pw},function(r){
 			if(r!="fail"){
@@ -42,17 +47,36 @@ var vm = avalon.define({
 			}
 		},'text');
 	},
-	loadCategory: function(p){
+	loadCategory: function(p,t){
 		vm.load(apiDomain+"admin/category.json",{"m.id": vm.userId, "m.token": vm.token, parent: p},function(list){
 			if(p==-1) vm.category1=list;
-			else vm.category2=list;
-		});
+			else if(!t) vm.category2=list;
+			else vm.category2Temp=list;
+		},'json');
+	},
+	editCategory: function(c){
+		c.edit=true;
+	},
+	saveCategory: function(c){
+		vm.load(apiDomain+"admin/saveCategory.json",{"m.id": vm.userId, "m.token": vm.token, "o.id": c.id, "o.parent": c.parent, "o.name": c.name},function(flag){
+			if(eval(flag)){
+				c.edit=false;
+			}
+		},'text');
+	},
+	statusCategory: function(c){
+		vm.load(apiDomain+"admin/statusCategory.json",{"m.id": vm.userId, "m.token": vm.token, "o.id": c.id, "o.parent": c.parent},function(flag){
+			if(eval(flag)){
+				c.status=!c.status;
+			}
+		},'text');
 	},
 	cat: function(el){
 		if(el.parent==-1){
 			vm.category1Cur=el;
 			vm.loadCategory(el.id);
 		}else{
+			vm.goodsTab='list';
 			vm.category2Cur=el;
 			vm.loadGoods(el.id);
 		}
@@ -60,7 +84,16 @@ var vm = avalon.define({
 	loadGoods: function(c2){
 		vm.load(apiDomain+"admin/goods.json",{"m.id": vm.userId, "m.token": vm.token, category: c2},function(list){
 			vm.goodsList=list;
-		});
+		},'json');
+	},
+	goodsToggle: function(tab){
+		vm.goodsTab=tab;
+		if(tab=='add'){
+			vm.goods=avalon.mix({},goods);
+			vm.goods.category1=vm.category1Cur.id;
+			vm.goods.category2=vm.category2Cur.id;
+			vm.loadCategory(vm.goods.category1,false);
+		}
 	},
 	load: function(url,data,fun,type){}
 });
